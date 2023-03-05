9ec249d523694b7502116db9644f4578c68516bb
marc.rullan <marc.rullan@sleepiz.com>
2023-03-05 14:15:33 +0000

    add interface to ask questions to historical figures

--- virtual_mentor/streamlit_app.py
+++ virtual_mentor/streamlit_app.py
@@ -1,6 +1,22 @@
-import requests
-
 import streamlit as st
+import openai
+
+from virtual_mentor.utils import create_message, compute_message_cost, query_model
+
+
+historical_figures = [
+    "Albert Einstein",
+    "Napoleon Bonaparte",
+    "Barack Obama",
+    "Donald Trump",
+    "Elon Musk",
+    "Marie Curie",
+]
+
+HF_KEY = st.secrets["HF_KEY"]
+openai.api_key = st.secrets["OPENAI_KEY"]
+API_URL = "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+headers = {"Authorization": f"Bearer {HF_KEY}"}
 
 
 st.set_page_config(
@@ -9,22 +25,24 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-API_KEY = st.secrets["API_KEY"]
-API_URL = "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
-headers = {"Authorization": f"Bearer {API_KEY}"}
 
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+st.title("Your virtual mentor")
 
 with st.form(key='my_form'):
-    labels = st.multiselect('Choose your labels', ["bro", "sis"])
-    inputs = st.text_area('Enter your text for classification', "Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!")
-    payload = {
-        "inputs": inputs,
-        "parameters": {"candidate_labels": labels},
-    }
-    submitted = st.form_submit_button('Classify!')
+    historical_character = st.selectbox("Choose a historical figure", historical_figures)
+    context_msg_content = f"You are {historical_character}. Represent yourself in a way that is\
+        consistent with your historical reputation."
+
+    question_msg_content = st.text_area(f'Enter your question for {historical_character}',
+                          "What is your opinion on the current political situation?")
+    messages=[
+            create_message('system', context_msg_content),
+            create_message('user', question_msg_content),
+        ]
+
+    submitted = st.form_submit_button('Submit the question!')
     if submitted:
-        output = query(payload)
-        st.write(output)
+        response = query_model(messages)
+        message_cost = compute_message_cost(messages)
+        st.write(f"Message cost: {message_cost}")
+        st.write(response['choices'][0]['message']['content'])
